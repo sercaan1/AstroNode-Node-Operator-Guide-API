4b0d93ffa283fefd49d3279356c9b6d668a84985
sercaan1 <sercankarakuyu@gmail.com>
2022-09-01 23:43:16 +0300

    api layer added

---
+++ Replica.API/Controllers/NodeController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstracts;
+using Dtos.Nodes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Replica.API.Controllers
+{
+    [ApiController]
+    public class NodeController : Controller
+    {
+        private readonly INodeService _nodeService;
+
+        public NodeController(INodeService nodeService)
+        {
+            _nodeService = nodeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] NodeCreateDto nodeCreateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var creationResult = await _nodeService.AddAsync(nodeCreateDto);
+
+                if (creationResult.IsSuccess)
+                    return Ok(creationResult.Data);
+
+                return BadRequest(creationResult.Data);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
