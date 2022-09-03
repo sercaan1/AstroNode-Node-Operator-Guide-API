048e06765428d709aa128a420bae80b3caef17c7
sercaan1 <sercankarakuyu@gmail.com>
2022-09-03 17:56:55 +0300

    swagger options removed

--- Replica.API/Program.cs
+++ Replica.API/Program.cs
@@ -19,12 +19,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers().AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(typeof(IProfile).Assembly);
 
